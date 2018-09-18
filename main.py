f3af2192f1e3765844253ac131cbe36975e4a0c9
liamgak <gyuhyeon1999@gmail.com>
2018-09-18 15:31:47 +0900

    third commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, send_file
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hci')
+def hci():
+    return render_template('HCI.html')
+
+if __name__ == '__main__':
+    app.run()
+
